3c4ba5a73b2a8c07eaf77baefcf070fd7e63addc
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-04-23 12:45:07 -0500

    empezando js y cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log("wake un Neo");
+console.log("hola mundo");
+
+const cerrarCart = document.getElementById("x-btn")
+const cart = document.querySelectorAll(".cart")
+
+
+cerrarCart.addEventListener("click", () =>{
+    if(5===5) return
+
+        cart.forEach(element => {
+            
+        (element => {
+            element.classlist.add("hidden")
+        })
+})
+})
